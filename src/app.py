4ca8604d159401b2123649a97bb9d09a9ba89de8
Fanhao Yu <fy229@cornell.edu>
2025-04-29 17:53:16 -0400

    Implement db.py and app.py
    - full base functionality with users, restaurants, and food items
    - implemented relationships in db
    - implemented routes

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from db import db
 from flask import Flask, request
 import json
-from db import Courses, Users, Assignments
+from db import Restaurant, User, Food
 
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "munch.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -14,6 +14,211 @@ app.config["SQLALCHEMY_ECHO"] = False
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+# User endpoints
+
+
+@app.route("/api/users/")
+def get_all_users():
+    """
+    Gets all users in the DB
+    """
+    users = []
+    for user in User.query.all():
+        users.append(user.serialize())
+    return json.dumps({"users": users}), 200
+
+
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    """
+    Creates a new user in the DB
+    """
+    body = json.loads(request.data)
+    username = body.get("username")
+    password = body.get("password")
+    email = body.get("email")
+    phone = body.get("phone")
+    if username is None or password is None or email is None or phone is None:
+        return json.dumps({"error": "Invalid input"}), 400
+    new_user = User(**body)
+    db.session.add(new_user)
+    db.session.commit()
+    return json.dumps(new_user.serialize()), 201
+
+
+@app.route("/api/users/<int:user_id>/")
+def get_user_by_id(user_id):
+    """
+    Gets a user by id from DB
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return json.dumps({"error": "User not found!"}), 404
+    return json.dumps(user.serialize()), 200
+
+
+@app.route("/api/users/<int:user_id>/", methods=["DELETE"])
+def delete_user_by_id(user_id):
+    """
+    Deletes a user by its id from DB
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return json.dumps({"error": 404})
+    db.session.delete(user)
+    db.session.commit()
+    return json.dumps(user.serialize()), 200
+
+
+# Restaurant endpoints
+
+
+@app.route("/api/restaurants/")
+def get_all_restaurants():
+    """
+    Gets all restaurants in the DB
+    """
+    restaurants = []
+    for restaurant in Restaurant.query.all():
+        restaurants.append(restaurant.serialize())
+    return json.dumps({"restaurants": restaurants}), 200
+
+
+@app.route("/api/restaurants/", methods=["POST"])
+def create_restaurant():
+    """
+    Creates a new restaurant in the DB
+    """
+    body = json.loads(request.data)
+    name = body.get("name")
+    address = body.get("address")
+    if name is None or address is None:
+        return json.dumps({"error": "Invalid input"}), 400
+    new_restaurant = Restaurant(**body)
+    db.session.add(new_restaurant)
+    db.session.commit()
+    return json.dumps(new_restaurant.serialize()), 201
+
+
+@app.route("/api/restaurants/<int:restaurant_id>/")
+def get_restaurant_by_id(restaurant_id):
+    """
+    Gets a restaurant by its id from DB
+    """
+    restaurant = Restaurant.query.filter_by(id=restaurant_id).first()
+    if restaurant is None:
+        return json.dumps({"error": "Restaurant not found"}), 404
+    return json.dumps(restaurant.serialize()), 200
+
+
+@app.route("/api/restaurants/<int:restaurant_id>/", methods=["DELETE"])
+def delete_restaurant_by_id(restaurant_id):
+    """
+    Deletes a restaurant by its id from DB
+    """
+    restaurant = User.query.filter_by(id=restaurant_id).first()
+    if restaurant is None:
+        return json.dumps({"error": 404})
+    db.session.delete(restaurant)
+    db.session.commit()
+    return json.dumps(restaurant.serialize()), 200
+
+
+# Food endpoints
+
+
+@app.route("/api/food/")
+def get_all_food():
+    """
+    Gets all food items in the DB
+    """
+    foods = []
+    for food in Food.query.all():
+        foods.append(food.serialize())
+    return json.dumps({"food items": foods}), 200
+
+
+@app.route("/api/restaurants/<int:restaurant_id>/food/", methods=["POST"])
+def create_food(restaurant_id):
+    """
+    Creates a new food and adds it to a restaurant
+    """
+    restaurant = Restaurant.query.filter_by(id=restaurant_id).first()
+    if restaurant is None:
+        return json.dumps({"error": "Restaurant not found!"}), 404
+    body = json.loads(request.data)
+    name = body.get("name")
+    price = body.get("price")
+    category = body.get("category")
+    if name is None or price is None or category is None:
+        return json.dumps({"error": "Invalid input"}), 400
+    new_food = Food(
+        name=name, price=price, category=category, restaurant_id=restaurant_id
+    )
+    db.session.add(new_food)
+    db.session.commit()
+    return json.dumps(new_food.serialize()), 201
+
+
+@app.route("/api/food/<int:food_id>/")
+def get_food_by_id(food_id):
+    """
+    Gets a food item by id from DB
+    """
+    food = Food.query.filter_by(id=food_id).first()
+    if food is None:
+        return json.dumps({"error": "Food not found!"}), 404
+    return json.dumps(food.serialize()), 200
+
+
+@app.route("/api/users/<int:user_id>/food/", methods=["POST"])
+def add_food_to_user(user_id):
+    """
+    Assigns a food to a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return json.dumps({"error": "User not found!"}), 404
+    body = json.loads(request.data)
+    food_id = body.get("food_id")
+    food = Food.query.filter_by(id=food_id).first()
+    if food is None:
+        return json.dumps({"error": "Food not found!"}), 404
+    user.foods.append(food)
+    db.session.commit()
+    return json.dumps(user.serialize()), 200
+
+
+@app.route("/api/users/<int:user_id>/favorites/", methods=["POST"])
+def add_favorite(user_id):
+    """
+    Assigns a food to a user's favorite foods
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return json.dumps({"error": "User not found!"}), 404
+    body = json.loads(request.data)
+    food_id = body.get("food_id")
+    food = Food.query.filter_by(id=food_id).first()
+    if food is None:
+        return json.dumps({"error": "Food not found!"}), 404
+    user.favorite_foods.append(food)
+    db.session.commit()
+    return json.dumps(user.serialize()), 200
+
+
+@app.route("/api/users/<int:user_id>/favorites/")
+def get_favorites(user_id):
+    """
+    Gets all favorited items of a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return json.dumps({"error": "User not found!"}), 404
+
+    favorited = [food.serialize() for food in user.favorite_foods]
+    return json.dumps({"favorited_foods": favorited}), 200
 
 
 if __name__ == "__main__":
--- src/db.py
+++ src/db.py
@@ -21,51 +21,70 @@ user_food_association_table = db.Table(
 class Restaurant(db.Model):
     __tablename__ = "restaurants"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
     menu = db.relationship("Food", cascade="delete")
 
     def __init__(self, **kwargs):
+        self.name = kwargs.get("name", "")
         self.address = kwargs.get("address", "")
-        self.menu = kwargs.get("menu", "")
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "address": self.address,
-            "menu": self.menu,
-            # "assignments": [a.serialize_no_courses() for a in self.assignments],
-            # "instructors": [i.serialize_no_courses() for i in self.instructors],
-            # "students": [s.serialize_no_courses() for s in self.students],
+            "menu": [food.simple_serialize() for food in self.menu],
         }
 
-    # def serialize2(self):
-    #     return {
-    #         "id": self.id,
-    #         "code": self.code,
-    #         "name": self.name,
-    #     }
+    def simple_serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+        }
 
 
 class Food(db.Model):
     __tablename__ = "foods"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String, nullable=False)
-    restuarant = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
+    restaurant_id = db.Column(
+        db.Integer, db.ForeignKey("restaurants.id"), nullable=False
+    )
     users = db.relationship(
         "User", secondary=user_food_association_table, back_populates="foods"
     )
     favorited_by = db.relationship(
-        "User", seconary=favorites_table, back_populates="favorite_foods"
+        "User", secondary=favorites_table, back_populates="favorite_foods"
     )
 
     def __init__(self, **kwargs):
         self.name = kwargs.get("name", "")
         self.price = kwargs.get("price", 0)
         self.category = kwargs.get("category", 0)
+        self.restaurant_id = kwargs.get("restaurant_id", 0)
 
-    # def serialize
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "category": self.category,
+            "restaurant": Restaurant.query.filter_by(id=self.restaurant_id)
+            .first()
+            .simple_serialize(),
+        }
+
+    def simple_serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "category": self.category,
+        }
 
 
 class User(db.Model):
@@ -88,4 +107,12 @@ class User(db.Model):
         self.email = kwargs.get("email", "")
         self.phone = kwargs.get("phone", 0)
 
-    # def serialize(self):
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "phone": self.phone,
+            "foods": [food.simple_serialize() for food in self.foods],
+            "favorites": [food.simple_serialize() for food in self.favorite_foods],
+        }
